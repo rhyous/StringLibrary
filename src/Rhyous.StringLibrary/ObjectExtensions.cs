abd446a818bf1c6c422eb10840b93f2a5dde742d
rhyous <rhyous@yahoo.com>
2016-09-22 16:10:42 -0600

    Added Unit Tests

--- src/Rhyous.StringLibrary/ObjectExtensions.cs
+++ src/Rhyous.StringLibrary/ObjectExtensions.cs
@@ -2,11 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace Rhyous.StringLibrary
 {
     public static class ObjectExtensions
     {
+        public static List<Type> SupportedGenericTypes = new List<Type> {
+            typeof(IList),
+            typeof(IDictionary),
+            typeof(IList<>),
+            typeof(IDictionary<,>)
+        };
+
         /// <summary>
         /// Finds any property of type string and trims it, unless that property has the
         /// IgnoreTrimAttribute applied.
@@ -14,57 +22,150 @@ namespace Rhyous.StringLibrary
         /// <param name="obj"></param>
         public static void TrimStringProperties(this object obj)
         {
-            var stringProperties = obj.GetType().GetProperties()
-                          .Where(p => p.PropertyType == typeof(string)
-                              && !Attribute.IsDefined(p, typeof(IgnoreTrim)));
-
-            foreach (var stringProperty in stringProperties)
-            {
-                var currentValue = (string)stringProperty.GetValue(obj, null);
-                if (currentValue != null)
-                    stringProperty.SetValue(obj, currentValue.TrimAll(), null);
-            }
-
-            if (IsGenericList(obj))
-            {
-                var enumerable = obj as IEnumerable;
-                if (enumerable != null)
-                {
-                    foreach (var item in enumerable)
-                    {
-                        item.TrimStringProperties();
-                    }
-                }
+            var type = obj.GetType();
+            if (!type.IsTrimmable() || type == typeof(string)) // We can't trim a string if it is the root object as a string is immutable
                 return;
-            }
 
-            var subClasses = obj.GetType().GetProperties()
-                .Where(p => p.PropertyType.IsClass
-                            && p.PropertyType != typeof(string)
-                            && !Attribute.IsDefined(p, typeof(IgnoreTrim)))
-                            .Select(p => p.GetValue(obj, null)).Where(c => c != null);
-            foreach (var subClass in subClasses)
+            if (type.IsArray)
+                obj.TrimArray();
+
+            if (obj.IsSupportedGeneric())
+                obj.TrimGeneric();
+
+            var properties = obj.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.SetProperty);
+            foreach (var prop in properties)
             {
-                if (subClass.GetType().IsPrimitive || subClass.GetType().IsArray || (subClass.GetType().IsGenericType && subClass.GetType().GetGenericTypeDefinition() == typeof(List<>)))
+                if (!prop.IsTrimmable())
                 {
                     continue;
                 }
-                subClass.TrimStringProperties();
+                if (prop.PropertyType == typeof(object))
+                {
+                    obj.TrimObject(prop);
+                    continue;
+                }
+                if (prop.PropertyType == typeof(string))
+                {
+                    obj.TrimString(prop);
+                    continue;
+                }
+                if (prop.PropertyType.IsSupportedGeneric())
+                {
+                    var generic = prop.GetValue(obj, null);
+                    generic.TrimGeneric();
+                    continue;
+                }
+
+                prop.GetValue(obj, null).TrimStringProperties();
             }
         }
 
-        public static bool IsGenericList(this object o)
+        public static void TrimArray(this object obj)
         {
-            var oType = o.GetType();
-            while (oType != null)
+            var array = obj as Array;
+            if (array != null & array.Length == 0)
+                return;
+            array.TrimList();
+        }
+
+        private static void TrimList(this IList array)
+        {
+            for (int i = 0; i < array.Count; i++)
             {
-                if (oType.IsGenericType && (oType.GetGenericTypeDefinition() == typeof(List<>)))
+                var item = array[i];
+                if (item != null && item.GetType() == typeof(string))
+                    array[i] = item.ToString().TrimAll();
+                else
+                    item.TrimStringProperties();
+            }
+        }
+
+        public static void TrimGeneric(this object obj)
+        {
+            var dictionary = obj as IDictionary;
+            if (dictionary != null)
+            {
+                TrimDictionary(dictionary);
+                return;
+            }
+            var list = obj as IList;
+            if (list != null)
+            {
+                list.TrimList();
+                return;
+            }
+        }
+
+        private static void TrimDictionary(IDictionary dictionary)
+        {
+            if (dictionary == null)
+                return;
+            var list = new List<object>();
+            foreach (var item in dictionary.Keys)
+                list.Add(item);
+            foreach (var key in list)
+            {
+                var value = dictionary[key];
+                var type = value.GetType();
+                if (type == typeof(string))
+                {
+                    dictionary[key] = ((string)dictionary[key]).TrimAll();
+                }
+                else
+                {
+                    if (type.IsTrimmable() && type.IsSupportedGeneric())
+                        dictionary[key].TrimStringProperties();
+                }
+            }
+        }
+
+        public static void TrimObject(this object obj, PropertyInfo prop)
+        {
+            var value = prop.GetValue(obj, null);
+            if (value.GetType() == typeof(string))
+                obj.TrimString(prop);
+            else
+                value.TrimStringProperties();
+        }
+
+        public static void TrimString(this object obj, PropertyInfo stringProperty)
+        {
+            string currentValue;
+            if (stringProperty.GetSetMethod() == null || (currentValue = (string)stringProperty.GetValue(obj, null)) == null)
+                return;
+            stringProperty.SetValue(obj, currentValue.TrimAll(), null);
+        }
+
+        public static bool IsTrimmable(this PropertyInfo pi)
+        {
+            if (pi == null)
+                return false;
+            var indexParams = pi.GetIndexParameters();
+            return (indexParams == null || indexParams.Length == 0 && pi.PropertyType.IsTrimmable());
+        }
+
+        internal static bool IsTrimmable(this Type type)
+        {
+            return !type.IsPrimitive && (!type.IsGenericType || type.IsSupportedGeneric());
+        }
+
+        internal static bool IsSupportedGeneric(this object o)
+        {
+            return o.GetType().IsSupportedGeneric();
+        }
+
+        internal static bool IsSupportedGeneric(this Type type)
+        {
+            while (type != null)
+            {
+                if (SupportedGenericTypes.Contains(type)
+                    || (type.IsGenericType && (SupportedGenericTypes.Contains(type.GetGenericTypeDefinition())))
+                    || type.GetInterfaces().Any(i => SupportedGenericTypes.Contains(i)))
                 {
                     return true;
                 }
-                oType = oType.BaseType;
+                type = type.BaseType;
             }
-
             return false;
         }
     }
--- src/Unit Tests/Rhyous.StringLibrary.Tests/TrimPropertiesTests.cs
+++ src/Unit Tests/Rhyous.StringLibrary.Tests/TrimPropertiesTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Rhyous.StringLibrary.Tests
 {
@@ -20,9 +21,28 @@ namespace Rhyous.StringLibrary.Tests
             public string String2 { get; set; }
         }
 
-        class ObjectList : List<object>
+        public class Getter
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Get { get { return " This cannot be set and cannot be trimmed. "; } }
+        }
+
+        public class ObjectList : List<object>
         {
             public string Name { get; set; }
+        }
+
+        public class ParentOfDictionary
+        {
+            public string Name { get; set; }
+            public Dictionary<string, object> Map { get; set; } = new Dictionary<string, object>();
+        }
+
+        public class ObjectProperties
+        {
+            public string Name { get; set; }
+            public object StringAsObject { get; set; }
         }
 
         [TestMethod]
@@ -52,7 +72,20 @@ namespace Rhyous.StringLibrary.Tests
             // Assert
             Assert.AreEqual(expected, testObj.Sub.String2);
         }
-        
+
+        [TestMethod]
+        public void GetterOnlyPropertiesAreIgnored()
+        {
+            // Arrange
+            var testObj = new Getter();
+
+            // Act
+            testObj.TrimStringProperties();
+
+            // Assert
+            Assert.IsTrue(testObj.Get.StartsWith(" "));
+        }
+
         [TestMethod]
         public void StringPropertyOfGenericListIsTrimmed()
         {
@@ -64,6 +97,62 @@ namespace Rhyous.StringLibrary.Tests
 
             // Assert - doesn't throw exception and regular stuff is trimmed
             Assert.AreEqual("List Name", testObj.Name);
+        }
+
+        [TestMethod]
+        public void ItemInGenericListIsTrimmed()
+        {
+            // Arrange
+            var testObj = new ObjectList { Name = " List Name " };
+            testObj.Add(" Trim me ");
+
+            // Act
+            testObj.TrimStringProperties();
+
+            // Assert
+            Assert.AreEqual("List Name", testObj.Name);
+            Assert.AreEqual("Trim me", testObj[0]);
+        }
+
+        [TestMethod]
+        public void EmptyDictionaryObjectsAreIgnored()
+        {
+            // Arrange
+            var testObj = new ParentOfDictionary { Name = " List Name " };
+
+            // Act
+            testObj.TrimStringProperties();
+
+            // Assert - doesn't throw exception and regular stuff is trimmed
+            Assert.AreEqual("List Name", testObj.Name);
+        }
+
+        [TestMethod]
+        public void DictionaryObjectsAreTrimmed()
+        {
+            // Arrange
+            var testObj = new ParentOfDictionary();
+            testObj.Map.Add("1", " Bad String ");
+
+            // Act
+            testObj.TrimStringProperties();
+
+            // Assert
+            Assert.AreEqual("Bad String", testObj.Map.Values.First());
+        }
+
+        [TestMethod]
+        public void ObjectsOfTypeStringAreTrimmed()
+        {
+            // Arrange
+            var testObj = new ObjectProperties { Name = " List Name ", StringAsObject = " Trim me " };
+
+            // Act
+            testObj.TrimStringProperties();
+
+            // Assert - doesn't throw exception and regular stuff is trimmed
+            Assert.AreEqual("List Name", testObj.Name);
+            Assert.AreEqual("Trim me", testObj.StringAsObject);
         }
     }
 }
