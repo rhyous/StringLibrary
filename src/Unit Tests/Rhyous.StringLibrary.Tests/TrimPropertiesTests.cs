7e699eaf818c9533d4240fdaaf29b74a56dce1c5
rhyous <rhyous@yahoo.com>
2016-09-24 00:50:27 -0600

    Remove unused usings

--- src/Unit Tests/Rhyous.StringLibrary.Tests/TrimPropertiesTests.cs
+++ src/Unit Tests/Rhyous.StringLibrary.Tests/TrimPropertiesTests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
 
 namespace Rhyous.StringLibrary.Tests
