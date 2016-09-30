5f1fe6b054b82061ccd0ae16f6dd66c1e06ad30d
rhyous <rhyous@yahoo.com>
2016-09-30 09:29:26 -0600

    Added Globalization

---
+++ src/Rhyous.StringLibrary/Globalization/GlobalizationStringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Rhyous.StringLibrary.Diacritics
+{
+    public static class GlobalizationStringExtensions
+    {
+        /// <summary>
+        /// Removes diacritics from a string.
+        /// </summary>
+        /// <param name="stIn"></param>
+        /// <returns>string without diacitics</returns>
+        public static string RemoveDiacritics(this string inString)
+        {
+            var strFormD = inString.Normalize(NormalizationForm.FormD);
+            var sb = new StringBuilder();
+            foreach (char c in strFormD.Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark))
+            {
+                sb.Append(c);
+            }
+            return (sb.ToString().Normalize(NormalizationForm.FormC));
+        }
+    }
+}
