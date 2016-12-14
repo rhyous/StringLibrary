85e10c955f64a389a08d7dc8a5f032f266805bc7
rhyous <rhyous@yahoo.com>
2016-12-14 12:21:40 -0700

    Improved Primitive conversion extensions with tests

--- src/Rhyous.StringLibrary/Conversion/PrimitiveStringExtensions.cs
+++ src/Rhyous.StringLibrary/Conversion/PrimitiveStringExtensions.cs
@@ -1,86 +1,82 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Globalization;
 
 namespace Rhyous.StringLibrary
 {
     public static class PrimitiveStringExtensions
     {
-        internal delegate bool TryParseMethod<T>(string s, out T result);
-
-        internal static T TryParse<T>(string value, TryParseMethod<T> handler, T defaultValue)
+        public static T To<T>(this string s, T defaultValue = default(T))
             where T : struct, IComparable, IConvertible, IComparable<T>, IEquatable<T>
         {
-            value = value.TrimAll();
-            if (string.IsNullOrEmpty(value))
-                return defaultValue;
-            T result;
-            if (handler(value, out result))
-                return result;
-            return defaultValue;
+            TypeConverter converter = TypeDescriptor.GetConverter(typeof(T));
+            try { return (T)converter.ConvertFromString(null, CultureInfo.InvariantCulture, s); }
+            catch { return defaultValue; }
         }
 
         public static byte ToByte(this string s, byte defaultValue = 0)
         {
-            return TryParse(s, byte.TryParse, defaultValue);
+            return To(s, defaultValue);
         }
 
         public static bool ToBool(this string s, bool defaultValue = false)
         {
-            return TryParse(s, bool.TryParse, defaultValue);
+            return To(s, defaultValue);
         }
 
         public static DateTime ToDate(this string s, DateTime defaultValue = default(DateTime))
         {
-            return TryParse(s, DateTime.TryParse, defaultValue);
+            return To(s, defaultValue);
         }
 
         public static decimal ToDecimal(this string s, decimal defaultValue = 0.0M)
         {
-            return TryParse(s, decimal.TryParse, defaultValue);
+            return To(s, defaultValue);
         }
 
         public static double ToDouble(this string s, double defaultValue = 0.0D)
         {
-            return TryParse(s, double.TryParse, defaultValue);
+            return To(s, defaultValue);
         }
 
         public static float ToFloat(this string s, float defaultValue = 0.0F)
         {
-            return TryParse(s, float.TryParse, defaultValue);
+            return To(s, defaultValue);
         }
 
         public static int ToInt(this string s, int defaultValue = 0)
         {
-            return TryParse(s, int.TryParse, defaultValue);
+            return To(s, defaultValue);
         }
 
         public static long ToLong(this string s, long defaultValue = 0L)
         {
-            return TryParse(s, long.TryParse, defaultValue);
+            return To(s, defaultValue);
         }
 
         public static sbyte ToSByte(this string s, sbyte defaultValue = 0)
         {
-            return TryParse(s, sbyte.TryParse, defaultValue);
+            return To(s, defaultValue);
         }
 
         public static short ToShort(this string s, short defaultValue = 0)
         {
-            return TryParse(s, short.TryParse, defaultValue);
+            return To(s, defaultValue);
         }
 
         public static uint ToUint(this string s, uint defaultValue = 0U)
         {
-            return TryParse(s, uint.TryParse, defaultValue);
+            return To(s, defaultValue);
         }
 
         public static ulong ToULong(this string s, ulong defaultValue = 0UL)
         {
-            return TryParse(s, ulong.TryParse, defaultValue);
+            return To(s, defaultValue);
         }
 
         public static ushort ToUShort(this string s, ushort defaultValue = 0)
         {
-            return TryParse(s, ushort.TryParse, defaultValue);
+            return To(s, defaultValue);
         }
     }
 }
---
+++ src/Unit Tests/Rhyous.StringLibrary.Tests/Conversion/PrimitiveStringExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Reflection;
+
+namespace Rhyous.StringLibrary.Tests.Comparison
+{
+    [TestClass]
+    public class PrimitiveStringExtensionsTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", @"Data\PrimitiveConversions.xml", "Row", DataAccessMethod.Sequential)]
+        public void ToGenericTest()
+        {
+            // Arrange
+            string s = TestContext.DataRow[0].ToString();
+            string strType = TestContext.DataRow[1].ToString();
+            var type = Type.GetType(strType);
+            string expected = TestContext.DataRow[2].ToString();
+            if (string.IsNullOrWhiteSpace(expected))
+                expected = s;
+
+            // Act
+            MethodInfo mi = typeof(PrimitiveStringExtensions).GetMethod("To");
+            MethodInfo method = mi.MakeGenericMethod(type);
+            var actual = method.Invoke(null, new object[] { s, null });
+
+            // Assert
+            Assert.AreEqual(type, actual.GetType());
+            Assert.AreEqual(expected, actual.ToString());
+        }
+    }
+}
