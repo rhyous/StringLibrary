4e3fcbd33ebc99c493a38b43ec629db23dd7a81a
rhyous <rhyous@yahoo.com>
2018-05-10 08:58:57 -0600

    Better handling of quotes.

--- src/Rhyous.StringLibrary.Shared/Wrap/WrapStringExtensions.cs
+++ src/Rhyous.StringLibrary.Shared/Wrap/WrapStringExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace Rhyous.StringLibrary
+﻿using System;
+using System.Linq;
+
+namespace Rhyous.StringLibrary
 {
     /// <summary>
     /// Extensions for Wrapping or Unwrapping strings
@@ -23,7 +26,43 @@
         /// <returns></returns>
         public static string Quote(this string value, char quote = '"')
         {
+            if (value.IsQuoted(new[] { quote.ToString() }))
+                return value;
             return value.Wrap(quote);
+        }
+
+        /// <summary>
+        /// Checks if a string is quoted.
+        /// </summary>
+        /// <param name="value">The string to check.</param>
+        /// <param name="quotes">The quote characters to check the prefix and suffix. Default is both a double quote and a single quote.</param>
+        /// <returns>bool</returns>
+        /// <remarks>Wrapped assumes both sides of the quote are the same. This is not quote: "value'. This is wrapped: "value". This is wrapped. 'value'.</remarks>
+        public static bool IsQuoted(this string value, params string[] quotes)
+        {
+            if (quotes == null || !quotes.Any())
+                quotes = new[] { "'", "\"" };
+            return value.IsWrapped(quotes);
+        }
+
+        /// <summary>
+        /// Checks if a string is quoted.
+        /// </summary>
+        /// <param name="value">The string to check.</param>
+        /// <param name="wraps">The wrap characters to check. An ArgumentNullException is thrown if this is null.</param>
+        /// <returns>bool</returns>
+        /// <remarks>Wrapped assumes both sides of the wrap are the same. This is not wrapped: #value!. This is wrapped: !value!. This is wrapped. #value#.</remarks>
+        public static bool IsWrapped(this string value, params string[] wraps)
+        {
+            if (wraps == null || !wraps.Any())
+                throw new ArgumentNullException("wrapChars");
+            foreach (var wrapChar in wraps)
+            {
+                var quoteCharAsString = wrapChar.ToString();
+                if (value.StartsWith(quoteCharAsString) && value.EndsWith(quoteCharAsString))
+                    return true;
+            }
+            return false;
         }
 
         /// <summary>
---
+++ src/Unit Tests/Rhyous.StringLibrary.Tests.Shared/Wrap/WrapStringExtensionsTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rhyous.StringLibrary.Tests.Shared.Wrap
+{
+    [TestClass]
+    public class WrapStringExtensionsTests
+    {
+        [TestMethod]
+        public void IsQuotedTrueSingleTest()
+        {
+            Assert.IsTrue("'QuotedString'".IsQuoted());
+        }
+
+        [TestMethod]
+        public void IsQuotedTrueDoubleTest()
+        {
+            Assert.IsTrue("\"QuotedString\"".IsQuoted());
+        }
+
+        [TestMethod]
+        public void UnquoteSingleTest()
+        {
+            // Arrange
+            var str = "'some string'";
+            var expected = "some string";
+
+            // Act
+            var actual = str.Unquote();
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void UnquoteDoubleTest()
+        {
+            // Arrange
+            var str = "\"some string\"";
+            var expected = "some string";
+
+            // Act
+            var actual = str.Unquote();
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void UnquoteNoQuotesDoesNothingTest()
+        {
+            // Arrange
+            var str = "some string";
+            var expected = "some string";
+
+            // Act
+            var actual = str.Unquote();
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void QuoteTest()
+        {
+            // Arrange
+            var str = "some string";
+            var expected = "\"some string\"";
+
+            // Act
+            var actual = str.Quote();
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void QuoteSingleTest()
+        {
+            // Arrange
+            var str = "some string";
+            var expected = "'some string'";
+
+            // Act
+            var actual = str.Quote('\'');
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void QuoteAlreadyQuotedTest()
+        {
+            // Arrange
+            var str = "\"some string\"";
+            var expected = "\"some string\"";
+
+            // Act
+            var actual = str.Quote();
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
