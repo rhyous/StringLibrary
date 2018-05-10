c4fb9fef21b3899a71bba04d52e57da2b4844da7
rhyous <rhyous@yahoo.com>
2018-05-10 08:56:48 -0600

    Another enum method with default value

--- src/Rhyous.StringLibrary.Shared/Enum/StringEnumExtensions.cs
+++ src/Rhyous.StringLibrary.Shared/Enum/StringEnumExtensions.cs
@@ -10,11 +10,25 @@ namespace Rhyous.StringLibrary
         {
             if (!typeof(T).IsEnum)
                 throw new ArgumentException("", "str");
-            T ret;
+            if (string.IsNullOrWhiteSpace(str))
+                return null;
             var isNumeric = str.All(c => Char.IsDigit(c));
-            if ((allowNumeric || !isNumeric) && Enum.TryParse(str, ignoreCase, out ret))
+            if ((allowNumeric || !isNumeric) && Enum.TryParse(str, ignoreCase, out T ret))
                 return ret;
             return null;
+        }
+
+        public static T ToEnum<T>(this string str, T defaultValue, bool ignoreCase = true, bool allowNumeric = true)
+            where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum)
+                throw new ArgumentException("T must be an enum", "T");
+            if (string.IsNullOrWhiteSpace(str))
+                return defaultValue;
+            var isNumeric = str.All(c => Char.IsDigit(c));
+            if ((allowNumeric || !isNumeric) && Enum.TryParse(str, ignoreCase, out T ret))
+                return ret;
+            return defaultValue;
         }
     }
 }
--- src/Unit Tests/Rhyous.StringLibrary.Tests.Shared/Enum/StringEnumExtensionsTests.cs
+++ src/Unit Tests/Rhyous.StringLibrary.Tests.Shared/Enum/StringEnumExtensionsTests.cs
@@ -1,15 +1,53 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Rhyous.StringLibrary.Tests.Shared.Enum
 {
     [TestClass]
     public class StringEnumExtensionsTests
     {
+        #region Nullable 
         [TestMethod]
-        public void TestStringToEnum()
+        public void TestStringToEnum_Nullable_Null_String()
+        {
+            // Arrange
+            string enumAsString = null;
+
+            // Act
+            var actual = enumAsString.ToEnum<TestEnum>();
+
+            // Assert
+            Assert.IsNull(actual);
+        }
+        
+        [TestMethod]
+        public void TestStringToEnum_Nullable_Empty_String()
+        {
+            // Arrange
+            var enumAsString = "";
+
+            // Act
+            var actual = enumAsString.ToEnum<TestEnum>();
+
+            // Assert
+            Assert.IsNull(actual);
+        }
+
+
+        [TestMethod]
+        public void TestStringToEnum_Nullable_Whitespace_String()
+        {
+            // Arrange
+            var enumAsString = "  \t";
+
+            // Act
+            var actual = enumAsString.ToEnum<TestEnum>();
+
+            // Assert
+            Assert.IsNull(actual);
+        }
+
+        [TestMethod]
+        public void TestStringToEnum_Nullable()
         {
             // Arrange
             var enumAsString = "Test1";
@@ -22,7 +60,7 @@ namespace Rhyous.StringLibrary.Tests.Shared.Enum
         }
 
         [TestMethod]
-        public void TestStringToEnumNumeric()
+        public void TestStringToEnum_Nullable_Numeric()
         {
             // Arrange
             var enumAsString = "0";
@@ -35,7 +73,7 @@ namespace Rhyous.StringLibrary.Tests.Shared.Enum
         }
 
         [TestMethod]
-        public void TestStringToEnumCaseSensitive()
+        public void TestStringToEnum_Nullable_CaseSensitive()
         {
             // Arrange
             var enumAsString = "test4";
@@ -48,7 +86,7 @@ namespace Rhyous.StringLibrary.Tests.Shared.Enum
         }
         
         [TestMethod]
-        public void TestStringToEnumNoNumericAllowed()
+        public void TestStringToEnum_Nullable_NoNumericAllowed()
         {
             // Arrange
             var enumAsString = "3";
@@ -59,5 +97,100 @@ namespace Rhyous.StringLibrary.Tests.Shared.Enum
             // Assert
             Assert.IsNull(actual);
         }
+        #endregion 
+
+        #region Default Value (not nullable)
+        [TestMethod]
+        public void TestStringToEnum_Null_String()
+        {
+            // Arrange
+            string enumAsString = null;
+
+            // Act
+            var actual = enumAsString.ToEnum(TestEnum.Test4);
+
+            // Assert
+            Assert.AreEqual(TestEnum.Test4, actual);
+        }
+
+        [TestMethod]
+        public void TestStringToEnum_Empty_String()
+        {
+            // Arrange
+            var enumAsString = "";
+
+            // Act
+            var actual = enumAsString.ToEnum(TestEnum.Test4);
+
+            // Assert
+            Assert.AreEqual(TestEnum.Test4, actual);
+        }
+
+
+        [TestMethod]
+        public void TestStringToEnum_Whitespace_String()
+        {
+            // Arrange
+            var enumAsString = "  \t";
+
+            // Act
+            var actual = enumAsString.ToEnum(TestEnum.Test4);
+
+            // Assert
+            Assert.AreEqual(TestEnum.Test4, actual);
+        }
+
+        [TestMethod]
+        public void TestStringToEnum()
+        {
+            // Arrange
+            var enumAsString = "Test1";
+
+            // Act
+            var actual = enumAsString.ToEnum(TestEnum.Test4);
+
+            // Assert
+            Assert.AreEqual(TestEnum.Test1, actual);
+        }
+
+        [TestMethod]
+        public void TestStringToEnumNumeric()
+        {
+            // Arrange
+            var enumAsString = "0";
+
+            // Act
+            var actual = enumAsString.ToEnum(TestEnum.Test4);
+
+            // Assert
+            Assert.AreEqual(TestEnum.Test1, actual);
+        }
+
+        [TestMethod]
+        public void TestStringToEnumCaseSensitive()
+        {
+            // Arrange
+            var enumAsString = "test4";
+
+            // Act
+            var actual = enumAsString.ToEnum(TestEnum.Test4, false);
+
+            // Assert
+            Assert.AreEqual(TestEnum.Test4, actual);
+        }
+
+        [TestMethod]
+        public void TestStringToEnumNoNumericAllowed()
+        {
+            // Arrange
+            var enumAsString = "3";
+
+            // Act
+            var actual = enumAsString.ToEnum(TestEnum.Test2, true, false);
+
+            // Assert
+            Assert.AreEqual(TestEnum.Test2, actual);
+        }
+        #endregion
     }
 }
