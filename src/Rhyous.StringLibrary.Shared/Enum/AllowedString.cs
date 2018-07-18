8e594f5aefa6e09dcdbfbad8d2ce0a7e06691b4e
rhyous <rhyous@yahoo.com>
2018-07-18 10:00:29 -0600

    AllowedString

---
+++ src/Rhyous.StringLibrary.Shared/Enum/AllowedString.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Rhyous.StringLibrary
+{
+    /// <summary>
+    /// A class that allows strings to act somewhat similar to enums, in that only certain strings can be allowed in set.
+    /// </summary>
+    public class AllowedString
+    {
+        /// <summary>
+        /// The constructor for the AllowedString
+        /// </summary>
+        /// <param name="allowedValues"></param>
+        /// <param name="defaultValue"></param>
+        public AllowedString(IEnumerable<string> allowedValues, string defaultValue = null)
+        {
+            if (allowedValues == null || !allowedValues.Any())
+                throw new ArgumentNullException("allowedValues", "You must specificy one or more allowed values.");
+            AllowedValues = new ReadOnlyCollection<string>(allowedValues.ToList());
+            DefaultValue = string.IsNullOrWhiteSpace(defaultValue) ? allowedValues.First() : defaultValue;
+            _Value = DefaultValue; // Default value could be a value that is outside of the allowed values.
+        }
+
+        /// <summary>
+        /// The property holding the current string value.
+        /// </summary>
+        public string Value
+        {
+            get { return _Value; }
+            set
+            {
+                if (AllowedValues.Contains(value))
+                    _Value = value;
+                throw new ValueNotAllowedException("value", $"Value ${value} is not allowed. Allowed values include: {string.Join(", ", AllowedValues)}");
+            }
+        } private string _Value;
+
+#if NET40
+        public ICollection<string> AllowedValues { get; }
+#else
+        public IReadOnlyCollection<string> AllowedValues { get; }
+#endif
+
+        /// <summary>
+        /// The propertly holding the default value.
+        /// </summary>
+        public string DefaultValue { get; }
+
+    }
+}
---
+++ src/Rhyous.StringLibrary.Shared/Enum/ValueNotAllowedException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Rhyous.StringLibrary
+{
+    public class ValueNotAllowedException : ArgumentException
+    {
+        public ValueNotAllowedException()
+        {
+        }
+
+        public ValueNotAllowedException(string message) : base(message)
+        {
+        }
+
+        public ValueNotAllowedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public ValueNotAllowedException(string message, string paramName) : base(message, paramName)
+        {
+        }
+
+        public ValueNotAllowedException(string message, string paramName, Exception innerException) : base(message, paramName, innerException)
+        {
+        }
+
+        protected ValueNotAllowedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
