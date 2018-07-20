a069624a0c823efdf85f530d6f9b15987349ae71
rhyous <rhyous@yahoo.com>
2018-07-20 10:33:20 -0600

    version to 1.1.6

--- src/Rhyous.StringLibrary.Net40-client/Properties/AssemblyInfo.cs
+++ src/Rhyous.StringLibrary.Net40-client/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.5.0")]
+[assembly: AssemblyFileVersion("1.1.6.0")]
 
 [assembly: InternalsVisibleTo("Rhyous.StringLibrary.Tests")]
