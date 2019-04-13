28f3059c6dc7ea1d307f742b3c479546b1dcb540
Jared A Barneck <rhyous@yahoo.com>
2019-04-12 19:09:52 -0600

    Added random string generator

---
+++ src/Rhyous.StringLibrary.Shared/Random/CryptoRandomString.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Rhyous.StringLibrary
+{
+    /// <summary>
+    /// A class that creates a Crypto random string using RNGCryptoServiceProvider.
+    /// </summary>
+    public class CryptoRandomString
+    {
+        /// <summary>A list of lowercase letters.</summary>
+        public const string LowerCase = "abcdefghijklmnopqrstuvwxyz";
+        /// <summary>A list of uppercase letters.</summary>
+        public const string UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        /// <summary>A list of numbers.</summary>
+        public const string Numbers = "0123456789";
+        /// <summary>A combination list of upper and lower case letters and numbers..</summary>
+        public const string AlphaNumeric = LowerCase + UpperCase + Numbers;
+
+        /// <summary>
+        /// A list of non-ambiguous alphanumeric numbers. For example, it might be hard to tell apart O, the letter, and 0, a zero, so those are excluded.
+        /// Excluded letters and numbers: 0, O, 1, l.
+        /// </summary>
+        public static string AlphaNumericNonAmbigous
+        {
+            get { return _AlphaNumericNonAmbiguous ?? (_AlphaNumericNonAmbiguous = new Regex("0|O|1|l").Replace(LowerCase + UpperCase + Numbers, "")); }
+        } private static string _AlphaNumericNonAmbiguous;
+
+        /// <summary>
+        /// Gets a crypto random string of the specified length.
+        /// </summary>
+        /// <param name="length">The length of the random string.</param>
+        /// <returns></returns>
+        public static string GetCryptoRandomBase64String(int length)
+        {
+            var buffer = new byte[length];
+            using (var rngCryptoServiceProvider = new RNGCryptoServiceProvider())
+            {
+                rngCryptoServiceProvider.GetNonZeroBytes(buffer);
+            }
+            return Convert.ToBase64String(buffer);
+        }
+        
+        /// <summary>
+        /// Any character from ASCII decimal 32 (space), including alphanumeric and punctuation, 
+        /// to ASCII decimal 126 (~). This represents all characters that an english keyboard 
+        /// could easily type.
+        /// </summary>
+        /// <param name="length">The length of the random string.</param>
+        /// <returns>A random string made up of Alphanumeric characters.</returns>
+        public static string GetCryptoRandomBase95String(int length)
+        {
+            return GetCryptoRandomBaseNString(length, 95);
+        }
+
+        /// <summary>
+        /// Gets a crypto random string using base n.
+        /// </summary>
+        /// <param name="length">The length of the random string.</param>
+        /// <param name="baseN">The number of ASCII characters to use.</param>
+        /// <param name="asciiOffset">This determines which character in ASCII to start with.</param>
+        /// <param name="allowedCharacters">Characters that are allowed. If a random character is chosen and isn't allowed, a new random character is chosen.</param>
+        /// <returns>A random string.</returns>
+        public static string GetCryptoRandomBaseNString(int length, byte baseN, int asciiOffset = 32, char[] allowedCharacters = null)
+        {
+            var buffer = new byte[length];
+            var builder = new StringBuilder();
+
+            using (var rngCryptoServiceProvider = new RNGCryptoServiceProvider())
+            {
+                rngCryptoServiceProvider.GetBytes(buffer);
+                foreach (var b in buffer)
+                {
+                    var tmpbuff = new byte[] { b };
+                    int max = (baseN * (256 / baseN)) - 1; // minus 1 because we start at 0
+                    while (tmpbuff[0] > max)
+                    {
+                        rngCryptoServiceProvider.GetBytes(tmpbuff);
+                    }
+                    var singleChar = (allowedCharacters == null)
+                        ? ByteToBaseNChar(tmpbuff[0], baseN, asciiOffset) // Start at ascii 32 (space) by default
+                        : ByteToAllowedCharacter(tmpbuff[0], allowedCharacters);
+                    builder.Append(singleChar);
+                }
+            }
+            return builder.ToString();
+        }
+
+        /// <summary>
+        /// Gets a random string made up of Alphanumeric characters.
+        /// </summary>
+        /// <param name="length">The length of the random string.</param>
+        /// <param name="removeAmbiguous">By default this is false. If true, ambigous letters are excluded.</param>
+        /// <returns>A random string made up of Alphanumeric characters.</returns>
+        public static string GetCryptoRandomAlphaNumericString(int length, bool removeAmbiguous = false)
+        {
+            return GetCryptoRandomBaseNString(length, (byte)AlphaNumeric.Length, 0, removeAmbiguous
+                ? AlphaNumeric.ToCharArray()
+                : AlphaNumericNonAmbigous.ToCharArray());
+        }
+
+        /// <summary>
+        /// Converts a byte to a character of base N.
+        /// </summary>
+        /// <param name="b">A byte.</param>
+        /// <param name="baseN">The number of ASCII characters to use.</param>
+        /// <param name="asciiOffset">This determines which character in ASCII to start with.</param>
+        /// <returns></returns>
+        public static char ByteToBaseNChar(byte b, int baseN, int asciiOffset)
+        {
+            return (char)(b % baseN + asciiOffset);
+        }
+
+        /// <summary>
+        /// Converts a byte to an allowed character.
+        /// </summary>
+        /// <param name="b">A byte.</param>
+        /// <param name="allowedCharacters">a list of allowed characters.</param>
+        /// <returns></returns>
+        public static char ByteToAllowedCharacter(byte b, char[] allowedCharacters)
+        {
+            return allowedCharacters[(b % allowedCharacters.Length)];
+        }
+    }
+}
---
+++ src/Unit Tests/Rhyous.StringLibrary.Tests.Shared/Random/CryptoRandomStringTests.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Rhyous.StringLibrary.Tests
+{
+    [TestClass]
+    public class CryptoRandomStringTests
+    {
+        [TestMethod]
+        public void TestTestThatCorrectCharacterCountIsReturned()
+        {
+            const int length = 100;
+            var randomString = CryptoRandomString.GetCryptoRandomBase95String(length);
+            Assert.AreEqual(length, randomString.Length);
+        }
+
+        [TestMethod]
+        public void TestAllCharactersAreUsed()
+        {
+            const int length = 1000000;
+            var randomString = CryptoRandomString.GetCryptoRandomBase95String(length);
+            for (int i = 32; i < 126; i++)
+            {
+                char c = (char)i;
+                Assert.IsTrue(randomString.Contains(c.ToString()));
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Performance")]
+        public void TestPerformanceTenMillionCharacters()
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            const int length = 1000000;
+            CryptoRandomString.GetCryptoRandomBase95String(length);
+            watch.Stop();
+            Assert.IsTrue(watch.ElapsedMilliseconds < 1000);
+        } // Elapsed Milliseconds 320 (it fluxuated a few milliseconds each run) 
+
+        [TestMethod]
+        [TestCategory("Performance")]
+        public void TestPerformanceLoop()
+        {
+            Stopwatch watch = new Stopwatch();
+            const int length = 16;
+            watch.Start();
+            for (int i = 0; i < 100000; i++)
+            {
+                CryptoRandomString.GetCryptoRandomBase95String(length);
+            }
+            watch.Stop();
+            Assert.IsTrue(watch.ElapsedMilliseconds < 1000);
+        }
+
+        [TestMethod]
+        [TestCategory("Slow")]
+        public void TestDistributionInTenMillionCharacters()
+        {
+            const int length = 1000000;
+            const int distibution = length / 95;
+            int[] margins = new int[9500];
+            for (int j = 0; j < 100; j++)
+            {
+                var randomString = CryptoRandomString.GetCryptoRandomBase95String(length);
+                for (int i = 32; i < 127; i++)
+                {
+                    //int count = randomString.Count(c => c == i);
+                    int count = CountInstancesOfChar(randomString, (char)i);
+                    margins[(j * 95) + i - 32] = count;
+                }
+            }
+            Assert.IsTrue(Math.Abs(margins.Average() - distibution) < .5);
+        }
+
+        private int CountInstancesOfChar(string str, char c)
+        {
+            int count = 0;
+            char[] strArray = str.ToCharArray();
+            int length = str.Length;
+            for (int n = length - 1; n >= 0; n--)
+            {
+                if (strArray[n] == c)
+                    count++;
+            }
+            return count;
+        }
+
+        [TestMethod]
+        public void GetCryptoRandomAlphaNumericStringTest()
+        {
+            const int length = 100000;
+            var randomString = CryptoRandomString.GetCryptoRandomAlphaNumericString(length);
+            foreach (char c in randomString)
+            {
+                Assert.IsTrue(CryptoRandomString.AlphaNumeric.Contains(c));
+            }
+        }
+    }
+}
