82d999f9c317a79a8a3508bb7ba258b228e1a13a
Jared A Barneck <rhyous@yahoo.com>
2019-11-11 10:28:53 -0700

    Added StringConcat.WithSeparate static method. - Concatenates N strings with the specified separator - Handles the separator existing or not, as well as whitespace.

---
+++ src/Rhyous.StringLibrary.Shared/Concatinization/StringConcat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Rhyous.StringLibrary
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class StringConcat
+    {
+        /// <summary>
+        /// Concatenates two strings with a separator, and handles cases where the
+        /// separator may or may not already exist, as well as trimming whitespace.
+        /// </summary>
+        /// <param name="separator">The separator character.</param>
+        /// <param name="strings">The strings to combine.</param>
+        /// <returns>The strings trimmed, and combined with 1 separator between them.</returns>
+        /// <remarks>This should work similar to Path.Combine, only with the ability to choose the separator,
+        /// and no path rooting when subsequent strings start with a /.</remarks>
+        public static string WithSeparator(char separator, params string[] strings)
+        {
+            if (strings == null || strings.Length == 0)
+                throw new ArgumentNullException(nameof(strings));
+            string concatenatedString = null;
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(strings[i]))
+                    throw new ArgumentNullException(nameof(strings), $"Item {i} cannot be null.");
+                string current;
+                do
+                {
+                    current = strings[i];
+                    strings[i] = strings[i].Trim();
+                    strings[i] = i == 0 ? strings[i].TrimEnd(separator) 
+                                        : strings[i].Trim(separator);
+                } while (strings[i] != current);
+                concatenatedString = string.IsNullOrEmpty(concatenatedString)
+                                   ? strings[i]
+                                   : $"{concatenatedString}{separator}{strings[i]}";
+            }
+            return concatenatedString;
+        }
+    }
+}
---
+++ src/Unit Tests/Rhyous.StringLibrary.Tests.Shared/Concatinization/StringConcatTests.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Rhyous.StringLibrary.Tests.Comparison
+{
+    [TestClass]
+    public class StringConcatTests
+    {
+        #region Argument null exception tests
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConcatinizationExtensions_NullThrows_Test()
+        {
+            StringConcat.WithSeparator('/', null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConcatinizationExtensions_EmptyThrows_Test()
+        {
+            StringConcat.WithSeparator('/', new string[] { });
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConcatinizationExtensions_ThreeString_FirstNull_Test()
+        {
+            string s1 = null;
+            string s2 = "test2";
+            var s3 = "test3";
+            var separator = '/';
+
+            StringConcat.WithSeparator(separator, new[] { s1, s2, s3 });
+        }
+
+        [TestMethod]
+
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConcatinizationExtensions_ThreeString_MiddleNull_Test()
+        {
+            var s1 = "test1";
+            string s2 = null;
+            var s3 = "test3";
+            var separator = '/';
+
+            StringConcat.WithSeparator(separator, new[] { s1, s2, s3 });
+        }
+        #endregion
+
+        [TestMethod]
+        public void ConcatinizationExtensions_SingleString_NoSeparator_Test()
+        {
+            var s1 = "test";
+            var actual = StringConcat.WithSeparator('/', new string[] { s1 });
+            Assert.AreEqual(s1, actual);
+        }
+
+        [TestMethod]
+        public void ConcatinizationExtensions_TwoStrings_Separated_Test()
+        {
+            var s1 = "test1";
+            var s2 = "test2";
+            var separator = '/';
+            var expected = "test1/test2";
+            var actual = StringConcat.WithSeparator(separator, new[] { s1, s2 });
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ConcatinizationExtensions_ThreeStrings_Separated_Test()
+        {
+            var s1 = "test1";
+            var s2 = "test2";
+            var s3 = "test3";
+            var separator = '/';
+            var expected = "test1/test2/test3";
+            var actual = StringConcat.WithSeparator(separator, new[] { s1, s2, s3 });
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ConcatinizationExtensions_FrontSlashOnFirstStringNotTrimmed_Test()
+        {
+            var s1 = "/test1";
+            var s2 = "test2";
+            var s3 = "test3";
+            var separator = '/';
+            var expected = "/test1/test2/test3";
+            var actual = StringConcat.WithSeparator(separator, new[] { s1, s2, s3 });
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ConcatinizationExtensions_AlreadyHaveSeparatorBothSides_Test()
+        {
+            var s1 = "/test1/";
+            var s2 = "/test2/";
+            var s3 = "/test3/";
+            var separator = '/';
+            var expected = "/test1/test2/test3";
+            var actual = StringConcat.WithSeparator(separator, new[] { s1, s2, s3 });
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ConcatinizationExtensions_WhiteSpace_Test()
+        {
+            var s1 = "/test1 /";
+            var s2 = " /test2/ ";
+            var s3 = "/ test3/ \t";
+            var separator = '/';
+            var expected = "/test1/test2/test3";
+            var actual = StringConcat.WithSeparator(separator, new[] { s1, s2, s3 });
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ConcatinizationExtensions_WhiteSpaceMultipleSlashes_Test()
+        {
+            var s1 = "/test1 /   /";
+            var s2 = " /test2/   /";
+            var s3 = "/ test3/ \t/";
+            var separator = '/';
+            var expected = "/test1/test2/test3";
+            var actual = StringConcat.WithSeparator(separator, new[] { s1, s2, s3 });
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
