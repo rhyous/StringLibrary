534f2c5e178d7a8563fdadc84118f7c2dc2a596b
Jared A Barneck <rhyous@yahoo.com>
2019-11-11 10:28:00 -0700

    ContainsAny string extension method added - New methods: StartsWithAny, EndsWithAny, ContainsAny - Defaults to case sensitive, but allows StringComparison if ignoring case is desired.

--- src/Rhyous.StringLibrary.Shared/Comparison/StringComparisonExtensions.cs
+++ src/Rhyous.StringLibrary.Shared/Comparison/StringComparisonExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Rhyous.StringLibrary
 {
@@ -7,6 +8,58 @@ namespace Rhyous.StringLibrary
     /// </summary>
     public static class StringComparisonExtensions
     {
+        #region StartsWithAny
+        /// <summary>
+        /// Checks if a string starts with any of the following values.
+        /// </summary>
+        /// <param name="str">The string to check.</param>
+        /// <param name="values">The values.</param>
+        /// <returns>True if the string starts with any of the provided values, false otherwise.</returns>
+        public static bool StartsWithAny(this string str, params string[] values)
+        {
+            return values.Any(v => str.StartsWith(v));
+        }
+
+        /// <summary>
+        /// Checks if a string starts with any of the following values.
+        /// </summary>
+        /// <param name="str">The string to check.</param>
+        /// <param name="comparison">The StringComparison that Specifies the culture, case, and sort rules to be used.</param>
+        /// <param name="values">The values.</param>
+        /// <returns>True if the string starts with any of the provided values, false otherwise.</returns>
+        public static bool StartsWithAny(this string str, StringComparison comparison = StringComparison.OrdinalIgnoreCase, params string[] values)
+        {
+            return values.Any(v => str.StartsWith(v, comparison));
+        }
+        #endregion
+
+        #region EndsWithAny
+        /// <summary>
+        /// Checks if a string ends with any of the following values.
+        /// </summary>
+        /// <param name="str">The string to check.</param>
+        /// <param name="values">The values.</param>
+        /// <returns>True if the string ends with any of the provided values, false otherwise.</returns>
+        public static bool EndsWithAny(this string str, params string[] values)
+        {
+            return values.Any(v => str.EndsWith(v));
+        }
+
+        /// <summary>
+        /// Checks if a string ends with any of the following values.
+        /// </summary>
+        /// <param name="str">The string to check.</param>
+        /// <param name="comparison">The StringComparison that Specifies the culture, case, and sort rules to be used.</param>
+        /// <param name="values">The values.</param>
+        /// <returns>True if the string ends with any of the provided values, false otherwise.</returns>
+        public static bool EndsWithAny(this string str, StringComparison comparison = StringComparison.OrdinalIgnoreCase, params string[] values)
+        {
+            return values.Any(v => str.EndsWith(v, comparison));
+        }
+        #endregion
+
+
+        #region Contains
         /// <summary>
         /// This method allows you to check if a string contains another string
         /// but also pass in a StringComparison so you can choose to do this
@@ -14,11 +67,39 @@ namespace Rhyous.StringLibrary
         /// </summary>
         /// <param name="source">The source string.</param>
         /// <param name="toCheck">The string to use to check if the source contains it.</param>
-        /// <param name="comp">The string comparison.</param>
+        /// <param name="comparison">The StringComparison that Specifies the culture, case, and sort rules to be used.</param>
         /// <returns>True if the source contains the string to check, false otherwise.</returns>
-        public static bool Contains(this string source, string toCheck, StringComparison comp)
+        public static bool Contains(this string source, string toCheck, StringComparison comparison)
         {
-            return source.IndexOf(toCheck, comp) >= 0;
+            return source.IndexOf(toCheck, comparison) >= 0;
         }
+
+        /// <summary>
+        /// This method allows you to check if a string contains another string
+        /// but also pass in a StringComparison so you can choose to do this
+        /// by ignoring case.
+        /// </summary>
+        /// <param name="source">The source string.</param>
+        /// <param name="values">The strings to use to check if the source contains it.</param>
+        /// <returns>True if the source contains the string to check, false otherwise.</returns>
+        public static bool ContainsAny(this string source, params string[] values)
+        {
+            return values.Any(v => source.Contains(v));
+        }
+
+        /// <summary>
+        /// This method allows you to check if a string contains another string
+        /// but also pass in a StringComparison so you can choose to do this
+        /// by ignoring case.
+        /// </summary>
+        /// <param name="source">The source string.</param>
+        /// <param name="comparison">The StringComparison that Specifies the culture, case, and sort rules to be used.</param>
+        /// <param name="values">The string to use to check if the source contains it.</param>
+        /// <returns>True if the source contains the string to check, false otherwise.</returns>
+        public static bool ContainsAny(this string source, StringComparison comparison, params string[] values)
+        {
+            return values.Any(v => source.Contains(v, comparison));
+        }
+        #endregion
     }
 }
--- src/Unit Tests/Rhyous.StringLibrary.Tests.Shared/Comparison/StringComparisonExtensionsTests.cs
+++ src/Unit Tests/Rhyous.StringLibrary.Tests.Shared/Comparison/StringComparisonExtensionsTests.cs
@@ -6,6 +6,8 @@ namespace Rhyous.StringLibrary.Tests
     [TestClass]
     public class StringComparisonExtensionsTests
     {
+        #region Contains
+
         [TestMethod]
         public void StringComparisonExtensions_Contains_Tests()
         {
@@ -18,5 +20,88 @@ namespace Rhyous.StringLibrary.Tests
             // Assert
             Assert.IsTrue(actual);
         }
+
+        [TestMethod]
+        public void StringComparisonExtensions_ContainsAny_CaseSensitive_Tests()
+        {
+            // Arrange
+            var s = "abcdefg";
+
+            // Act
+            var actual = s.ContainsAny("CDE", "Efg");
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void StringComparisonExtensions_ContainsAny_CaseInsensitive_Tests()
+        {
+            // Arrange
+            var s = "abcdefg";
+
+            // Act
+            var actual = s.ContainsAny(StringComparison.OrdinalIgnoreCase, "CDE", "Efg");
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+        #endregion
+
+        #region StartsWithAny
+        [TestMethod]
+        public void StringComparisonExtensions_StartsWithAny_CaseSensitive_Tests()
+        {
+            // Arrange
+            var s = "abcdefg";
+
+            // Act
+            var actual = s.StartsWithAny("ABC", "AB");
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void StringComparisonExtensions_StartsWithAny_CaseInsensitive_Tests()
+        {
+            // Arrange
+            var s = "abcdefg";
+
+            // Act
+            var actual = s.StartsWithAny(StringComparison.OrdinalIgnoreCase, "ABC", "AB");
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+        #endregion
+
+        #region EndsWithAny
+        [TestMethod]
+        public void StringComparisonExtensions_EndsWithAny_CaseSensitive_Tests()
+        {
+            // Arrange
+            var s = "abcdefg";
+
+            // Act
+            var actual = s.EndsWithAny("EFG", "FG");
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void StringComparisonExtensions_EndsWithAny_CaseInsensitive_Tests()
+        {
+            // Arrange
+            var s = "abcdefg";
+
+            // Act
+            var actual = s.EndsWithAny(StringComparison.OrdinalIgnoreCase, "EFG", "FG");
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+        #endregion
     }
 }
