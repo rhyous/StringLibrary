5cedd3ccab558b5e1ae75d560adb7a5927898b98
Jared A Barneck <rhyous@yahoo.com>
2020-01-20 08:56:36 -0700

    Added ConcurrentHashSet to make USEnglishPluralizer more threadsafe

--- src/Rhyous.StringLibrary/Pluralization/Language/en-US/USEnglishSingularNounsEndingInOneS.cs
+++ src/Rhyous.StringLibrary/Pluralization/Language/en-US/USEnglishSingularNounsEndingInOneS.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Rhyous.StringLibrary.Pluralization
@@ -9,15 +10,41 @@ namespace Rhyous.StringLibrary.Pluralization
     /// If you see a noun missing, don't hesitate to ask for it to be added: https://github.com/rhyous/StringLibrary
     /// Either: 1. Fork, fix, request a pull request, or 2. Submit an Issue.
     /// </summary>
-    public class SingularNounsEndingInOneS : HashSet<string>
+    public class SingularNounsEndingInOneS : ICollection<string>
     {
+        internal ConcurrentHashSet<string> _HashSet = new ConcurrentHashSet<string>(StringComparer.OrdinalIgnoreCase);
+
         /// <summary>
         /// The constructor for this object.
         /// </summary>
-        public SingularNounsEndingInOneS() : base(StringComparer.OrdinalIgnoreCase)
+        public SingularNounsEndingInOneS()
         {
             Init();
         }
+
+        /// <inheritdoc />
+        public int Count => _HashSet.Count;
+
+        /// <inheritdoc />
+        public bool IsReadOnly => ((ICollection<string>)_HashSet).IsReadOnly;
+
+        /// <inheritdoc />
+        public void Add(string item) => _HashSet.Add(item);
+
+        /// <inheritdoc />
+        public void Clear() => _HashSet.Clear();
+
+        /// <inheritdoc />
+        public bool Contains(string item) => _HashSet.Contains(item);
+
+        /// <inheritdoc />
+        public void CopyTo(string[] array, int arrayIndex) => ((ICollection<string>)_HashSet).CopyTo(array, arrayIndex);
+
+        /// <inheritdoc />
+        public IEnumerator<string> GetEnumerator() => _HashSet.GetEnumerator();
+
+        /// <inheritdoc />
+        public bool Remove(string item) => ((ICollection<string>)_HashSet).Remove(item);
 
         /// <summary>
         /// A method to initialize this HashSet.
@@ -56,5 +83,7 @@ namespace Rhyous.StringLibrary.Pluralization
             Add("synopsis");
             Add("thesis");
         }
+
+        IEnumerator IEnumerable.GetEnumerator() => _HashSet.GetEnumerator();
     }
 }
