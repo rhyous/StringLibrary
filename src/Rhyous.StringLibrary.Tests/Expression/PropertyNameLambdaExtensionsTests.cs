281e4bf8feacad4c3bea3d7fadb04805cb0e6419
Jared Barneck <rhyous@yahoo.com>
2021-09-22 15:12:27 -0600

    Restore whitespace for class region

--- src/Rhyous.StringLibrary.Tests/Expression/PropertyNameLambdaExtensionsTests.cs
+++ src/Rhyous.StringLibrary.Tests/Expression/PropertyNameLambdaExtensionsTests.cs
@@ -55,7 +55,7 @@ namespace Rhyous.StringLibrary.Tests.Expression
             Assert.AreEqual(expectedResult, result, message);
         }
 
-#region Class
+        #region Class
         public class ModelA
         {
             public int Id { get; set; }
@@ -63,7 +63,7 @@ namespace Rhyous.StringLibrary.Tests.Expression
             public DateTime Date { get; set; }
             public Guid Guid { get; set; }
         }
-#endregion
+        #endregion
 
     }
 }
