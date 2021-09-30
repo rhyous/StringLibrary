ce6a7cce5ace61d9da491fc746a24ea63c320745
Jared Barneck <rhyous@yahoo.com>
2021-09-30 12:16:10 -0600

    Added stream.AsStringAsync() extension method

--- src/Rhyous.StringLibrary.Tests/Conversion/StreamStringExtensionsTests.cs
+++ src/Rhyous.StringLibrary.Tests/Conversion/StreamStringExtensionsTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
 
 namespace Rhyous.StringLibrary.Tests
 {
@@ -6,7 +7,7 @@ namespace Rhyous.StringLibrary.Tests
     public class StreamStringExtensionsTests
     {
         [TestMethod]
-        public void StreamStringExtensionsTest()
+        public void StreamStringExtensions_AsString_Test()
         {
             // Arrange
             var a = "abc";
@@ -14,6 +15,20 @@ namespace Rhyous.StringLibrary.Tests
             // Act
             var stream = a.ToStream();
             var b = stream.AsString();
+
+            // Assert
+            Assert.AreEqual(a, b);
+        }
+
+        [TestMethod]
+        public async Task StreamStringExtensions_AsStringAsync_Test()
+        {
+            // Arrange
+            var a = "abc";
+
+            // Act
+            var stream = a.ToStream();
+            var b = await stream.AsStringAsync();
 
             // Assert
             Assert.AreEqual(a, b);
