2d8e742f8bdd539b76b802376ce5b586627f8451
Jared A Barneck <rhyous@yahoo.com>
2021-09-30 12:27:01 -0600

    Added stream.AsStringAsync() extension method (#6)

--- src/Rhyous.StringLibrary/Conversion/StreamStringExtensions.cs
+++ src/Rhyous.StringLibrary/Conversion/StreamStringExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Rhyous.StringLibrary
 {
@@ -17,10 +18,7 @@ namespace Rhyous.StringLibrary
         /// <returns>A stream.</returns>
         public static Stream ToStream(this string str, Encoding encoding = null)
         {
-            if (encoding == null)
-            {
-                encoding = Encoding.UTF8; // Default
-            }
+            encoding = encoding ?? Encoding.UTF8; // Default
             return new MemoryStream(encoding.GetBytes(str ?? ""));
         }
 
@@ -32,13 +30,26 @@ namespace Rhyous.StringLibrary
         /// <returns>A string.</returns>
         public static string AsString(this Stream stream, Encoding encoding = null)
         {
-            if (encoding == null)
-            {
-                encoding = Encoding.UTF8; // Default
-            }
+            encoding = encoding ?? Encoding.UTF8; // Default
             using (var reader = new StreamReader(stream, encoding))
             {
                 return reader.ReadToEnd();
+            }
+        }
+
+        /// <summary>
+        /// Converts a stream to a string, asynchronously.
+        /// </summary>
+        /// <param name="stream">The stream.</param>
+        /// <param name="encoding">The encoding.</param>
+        /// <returns>A Task{string}. Use await or call task.Result to get the string.</returns>
+        public static async Task<string> AsStringAsync(this Stream stream, Encoding encoding = null)
+        {
+            encoding = encoding ?? Encoding.UTF8; // Default
+            
+            using (var reader = new StreamReader(stream, encoding))
+            {
+                return await reader.ReadToEndAsync();
             }
         }
     }
